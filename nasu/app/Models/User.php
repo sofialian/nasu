d9c49dedfea135099a50b8fd38467db9af48a400
sofia <sofial.garm@gmail.com>
2025-04-14 23:25:00 +0200

    tabla users

--- nasu/app/Models/User.php
+++ nasu/app/Models/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array<string>
      */
     protected $fillable = [
-        'username',             
+        'name',             
         'email',             
         'password',
         'exp',       
