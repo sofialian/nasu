b8dd166e3c043e57a86fde52ff0e1c78bbb4411b
sofia <sofial.garm@gmail.com>
2025-06-22 22:38:04 +0200

    imagenes seed

--- nasu/database/seeders/FurnitureViewSeeder.php
+++ nasu/database/seeders/FurnitureViewSeeder.php
@@ -10,7 +10,7 @@ class FurnitureViewSeeder extends Seeder
     public function run(): void
     {
         DB::table('furniture_views')->insert([
-               [
+            [
                 'furniture_id' => 1,
                 'view' => 'front',
                 'image_path' => 'furniture/cama-simple-front.png',
@@ -21,7 +21,7 @@ class FurnitureViewSeeder extends Seeder
             ],
 
 
-               [
+            [
                 'furniture_id' => 2,
                 'view' => 'front',
                 'image_path' => 'furniture/front-basic-desk.png',
@@ -42,8 +42,8 @@ class FurnitureViewSeeder extends Seeder
             ],
             [
                 'furniture_id' => 3,
-                'view' => 'left',
-                'image_path' => 'furniture/left-basic-chair.png',
+                'view' => 'right',
+                'image_path' => 'furniture/right-basic-chair.png',
                 'width' => 40,
                 'height' => 48,
                 'created_at' => now(),
@@ -51,8 +51,8 @@ class FurnitureViewSeeder extends Seeder
             ],
             [
                 'furniture_id' => 3,
-                'view' => 'right',
-                'image_path' => 'furniture/right-basic-chair.png',
+                'view' => 'left',
+                'image_path' => 'furniture/left-basic-chair.png',
                 'width' => 40,
                 'height' => 48,
                 'created_at' => now(),
@@ -68,7 +68,7 @@ class FurnitureViewSeeder extends Seeder
                 'updated_at' => now(),
             ],
 
-               [
+            [
                 'furniture_id' => 4,
                 'view' => 'front',
                 'image_path' => 'furniture/front-plant.png',
@@ -77,16 +77,7 @@ class FurnitureViewSeeder extends Seeder
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
-                [
-                'furniture_id' => 4,
-                'view' => 'left',
-                'image_path' => 'furniture/left-plant.png',
-                'width' => 48,
-                'height' => 48,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-                [
+            [
                 'furniture_id' => 4,
                 'view' => 'right',
                 'image_path' => 'furniture/right-plant.png',
@@ -95,10 +86,65 @@ class FurnitureViewSeeder extends Seeder
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
-                [
+            [
+                'furniture_id' => 4,
+                'view' => 'left',
+                'image_path' => 'furniture/left-plant.png',
+                'width' => 48,
+                'height' => 48,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
                 'furniture_id' => 4,
                 'view' => 'back',
                 'image_path' => 'furniture/back-plant.png',
+                'width' => 48,
+                'height' => 48,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'furniture_id' => 5,
+                'view' => 'front',
+                'image_path' => 'furniture/rug.png',
+                'width' => 48,
+                'height' => 48,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+            [
+                'furniture_id' => 6,
+                'view' => 'front',
+                'image_path' => 'furniture/front-simple-cupboard.png',
+                'width' => 48,
+                'height' => 48,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'furniture_id' => 6,
+                'view' => 'right',
+                'image_path' => 'furniture/right-simple-cupboard.png',
+                'width' => 48,
+                'height' => 48,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'furniture_id' => 6,
+                'view' => 'left',
+                'image_path' => 'furniture/left-simple-cupboard.png',
+                'width' => 48,
+                'height' => 48,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'furniture_id' => 6,
+                'view' => 'back',
+                'image_path' => 'furniture/back-simple-cupboard.png',
                 'width' => 48,
                 'height' => 48,
                 'created_at' => now(),
